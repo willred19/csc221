02c084ee01d01d764ce1b677e36fae6e4b7a1ab4
David O'Gwynn <dogwynn@acm.org>
2016-03-23 09:51:31 -0500

    added hw4

---
+++ hw/hw4/hw4.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import hw4_solution
+# --------------------------------------------------------------------
+# Problem 1
+# 
+# Create a function, is_odd, that takes a number and returns True if
+# that number is odd.
+# 
+# Function: is_odd
+# 
+# parameters:
+# - number
+#
+# returns: boolean
+
+
+
+# --------------------------------------------------------------------
+# Problem 2
+# 
+# Create a function, is_even, that takes a number and returns True if
+# that number is even. 
+# 
+# Function: is_even
+# 
+# parameters:
+# - number
+#
+# returns: boolean
+
+
+
+# --------------------------------------------------------------------
+# Problem 3
+# 
+# Create a function, is_mult_of_four, that takes a number and returns
+# True if that number is a multiple of four. 
+# 
+# Function: is_mult_of_four
+# 
+# parameters:
+# - number
+#
+# returns: boolean
+
+
+
+# --------------------------------------------------------------------
+# Problem 4
+# 
+# Create a function, is_mult_of_x, that takes a number and a divisor
+# and returns True if that number is divisible by divisor
+# 
+# Function: is_mult_of_divisor
+# 
+# parameters:
+# - number
+# - divisor
+#
+# returns: boolean
+
+
+
+# --------------------------------------------------------------------
+# Problem 5
+# 
+# Given a string s, return a string made of the first 2 and the last 2
+# chars of the original string, so 'spring' yields 'spng'. However, if
+# the string length is less than 2, return instead the empty string.
+#
+# Function: both_ends
+#
+# parameters:
+# - s
+#
+# returns: string
---
+++ hw/hw4/hw4_solution.py
@@ -0,0 +1,3 @@
+
+def is_odd(number):
+    return number%2==1
