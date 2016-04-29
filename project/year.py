8cffcb6feffde78e9c5d10ddbd65293d710c08f8
Will Redmond <williamredmond@students.belhaven.edu>
2016-04-29 14:04:52 +0000

    code review fix for year.py, fixed doctests

--- project/year.py
+++ project/year.py
@@ -1,17 +1,19 @@
 def is_leap_year(year):
+    '''
+    >>> is_leap_year(2000)
+    True
+    >>> is_leap_year(1985)
+    False
+    >>> is_leap_year(1900)
+    False
+    >>> is_leap_year(2007)
+    False
+    >>> is_leap_year(2016)
+    True
+    >>> is_leap_year(2006)
+    False
+    '''
     if year %4 == 0 and year %100 != 0 or year % 400 == 0:
         return True
     else:
         return False
-        '''
-    >>> test_is_leap_year(2000)
-    True
-    >>> test_is_leap_year(1985)
-    False
-    >>> test_is_leap_year(2007)
-    False
-    >>> test_is_leap_year(2016)
-    True
-    >>> test_is_leap_year(2006)
-    False
-        '''
